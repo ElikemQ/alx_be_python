9b886e537809760cff17c97965b8f8c9bee6a668
ElikemQ <elikemgbotsyo@gmail.com>
2024-12-22 23:57:45 +0000

    modified pattern_drawing

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -2,7 +2,11 @@ size = int(input("Enter the size of the pattern: "))
 if size <= 0:
     print("Please enter a positive integer")
 else:
-    for row in range(size):
-        for col in range(size):
+    row = 0
+    while row < size:
+        col = 0
+        while col < size:
             print("*", end = " ")
+            col +=1
         print()
+        row +=1
