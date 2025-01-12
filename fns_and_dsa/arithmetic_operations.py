5a0c6f4e5147ed8d64a6b0b2c7a90f9e28f26d54
ElikemQ <elikemgbotsyo@gmail.com>
2025-01-12 09:43:13 +0000

    modified the arithmetic_operations.py file to cater for divide by zero

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,10 +6,11 @@ def perform_operation(num1, num2, operation):
     elif operation == 'multiply':
         return f"Result: {num1 * num2}"
     elif operation == 'divide':
-        if num2 != 0:
-            return f"Result: {num1 / num2}"
-        else:
+        if num2 == 0:
             return('Can not divide by zero')
+        else:
+            return f"Result: {num1 / num2}"
+            
     else:
         return('Operation not found')
 
