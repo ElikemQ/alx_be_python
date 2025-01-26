3bad5b231d26ec60298e2874a024e01d74ca61d9
ElikemQ <elikemgbotsyo@gmail.com>
2025-01-26 22:10:38 +0000

    added polymorphism_dem.py to repo

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,23 @@
+class Shape:
+    def __init__(self):
+        pass
+    def area(self):
+        raise NotImplementedError("Derived classes must overide this method")
+    
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        super().__init__()
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+    
+class Circle(Shape):
+    def __init__(self, radius):
+        super().__init__()
+        self.radius = radius
+    
+    def area(self):
+        import math
+        return math.pi * (self.radius ** self.radius)
