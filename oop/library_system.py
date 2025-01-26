76611f770a9bed7423f89e4a46bb7715d6343ef4
ElikemQ <elikemgbotsyo@gmail.com>
2025-01-26 15:31:19 +0000

    modified library_systems

--- oop/library_system.py
+++ oop/library_system.py
@@ -4,7 +4,7 @@ class Book:
         self.author = author
 
     def __str__(self):
-        return f"Book: {self.title}, by {self.author}"
+        return f"Book: {self.title} by {self.author}"
     
 
 class EBook(Book):
@@ -13,7 +13,7 @@ class EBook(Book):
         self.file_size = file_size
 
     def __str__(self):
-        return f"EBook: {self.title} by {self.author} File Size: {self.file_size}KB"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
